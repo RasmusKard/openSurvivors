df6936e842c76856309b2736a15e42a1d11ccfed
P3-J <jako.keskkyla@gmail.com>
2023-10-09 20:43:59 +0300

    Big changes brothers

---
+++ assets/player/player.cs
@@ -0,0 +1,21 @@
+using Godot;
+using System;
+
+public partial class player : CharacterBody2D
+{
+	
+	[Export]
+	public int Speed { get; set; } = 400;
+
+	public void GetInput()
+	{
+		Vector2 inputDirection = Input.GetVector("left", "right", "up", "down");
+		Velocity = inputDirection * Speed ;
+	}
+
+	// Called every frame. 'delta' is the elapsed time since the previous frame.
+	public override void _PhysicsProcess(double delta) {
+			GetInput();
+			MoveAndSlide();
+	}
+}
